d3da5b3258c034ae78f647e411b383b55b04b281
AJ Rios <arios@wrbts.ads.wrberkley.com>
2023-09-05 17:32:53 -0400

    -added razor functionality

---
+++ PopsCarsSite/Pages/CarList.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Pop_s_Cars;
+
+namespace PopsCarsSite.Pages
+{
+    public class CarListComponent: ComponentBase
+    {
+        protected List<Cars> ListOfCars = new();
+        protected int carYear;
+        private Service service = new();
+        protected override async Task OnInitializedAsync()
+        {
+            ListOfCars = service.GetAllCars();
+        }
+        protected async Task FilterByYear()
+        {
+            ListOfCars = service.GetCarsByYear(carYear);
+            StateHasChanged();
+        }
+    }
+
+}
