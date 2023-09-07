78f913acda82437a212cd01bf342614f924b56a2
AJ Rios <arios@wrbts.ads.wrberkley.com>
2023-09-07 17:41:27 -0400

    -updated Carlist.Razor.cs and Carlist.razor

--- PopsCarsSite/Pages/CarList.razor.cs
+++ PopsCarsSite/Pages/CarList.razor.cs
@@ -3,38 +3,44 @@ using Pop_s_Cars;
 
 namespace PopsCarsSite.Pages
 {
-    public class CarListComponent: ComponentBase
-    {
-        protected List<Cars> ListOfCars = new();
-        protected int carYear;
-        protected string carModel;
-        protected string carMake;
-        protected string carColor;
-        private Service service = new();
-        protected override async Task OnInitializedAsync()
-        {
-            ListOfCars = service.GetAllCars();
-        }
-        protected async Task FilterByYear()
-        {
-            ListOfCars = await service.GetCarsByYear(carYear);
-            StateHasChanged();
-        }
+	public class CarListComponent : ComponentBase
+	{
+		protected List<Cars> ListOfCars = new();
+		protected int? carYear;
+		protected string carModel;
+		protected string carMake;
+		protected string carColor;
+		private Service service = new();
+		protected override async Task OnInitializedAsync()
+		{
+			ListOfCars = service.GetAllCars();
+		}
+		protected async Task FilterByYear()
+		{
+			if (carYear.HasValue)
+			{
+				ListOfCars = service.GetCarsByYear(carYear.Value);
+			}
+			else
+			{
+				ListOfCars = new();
+			}
+		}
 
-        protected async Task FilterByModel()
-        {
-            ListOfCars = await service.GetCarsByModel(carModel);
-        }
+		protected async Task FilterByModel()
+		{
+			ListOfCars = service.GetCarsByModel(carModel);
+		}
 
-        protected async Task FilterByMake()
-        {
-            ListOfCars = await service.GetCarsByMake(carMake);
-        }
+		protected async Task FilterByMake()
+		{
+			ListOfCars = service.GetCarsByMake(carMake);
+		}
 
-        protected async Task FilterByColor()
-        {
-            ListOfCars = await service.GetCarsByColor(carColor);
-        }
-    }
+		protected async Task FilterByColor()
+		{
+			ListOfCars = service.GetCarsByColor(carColor);
+		}
+	}
 
 }
