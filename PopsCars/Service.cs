b97054f8553c9f73033da0757d19591f1cd71eef
AJ <rios.alex@hotmail.com>
2023-09-13 23:26:38 -0400

    -changed the MainSearch method

--- PopsCars/Service.cs
+++ PopsCars/Service.cs
@@ -10,19 +10,22 @@ namespace PopsCars
             Data data = new Data();
             return data.carList;
         }
-      
-      public List<Cars> MainSearch(string search)
-{
-        Data data = new Data();
-        var searchResults = data.carList.Where(c =>
-        c.color.Contains(search, StringComparison.InvariantCultureIgnoreCase) ||
-        c.make.Contains(search, StringComparison.InvariantCultureIgnoreCase) ||
-        c.model.Contains(search, StringComparison.InvariantCultureIgnoreCase) ||
-        c.year.ToString().Contains(search)).ToList();
 
-        return searchResults;
-}
+        public List<Cars> MainSearch(string search)
+        {
+            Data data = new Data();
 
-        
+            var searchResults = data.carList.Where(c =>
+                search.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Any(term =>   //received help for line 19
+                    c.color.Contains(term, StringComparison.InvariantCultureIgnoreCase) ||
+                    c.make.Contains(term, StringComparison.InvariantCultureIgnoreCase) ||
+                    c.model.Contains(term, StringComparison.InvariantCultureIgnoreCase) ||
+                    c.year.ToString().Contains(term))
+            ).ToList();
+
+            return searchResults;
+        }
+
+
     }
 }
