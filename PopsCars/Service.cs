25860eeddd521c7f7c5091100fd9e6a6d695d75d
AJ <rios.alex@hotmail.com>
2023-09-13 22:33:23 -0400

    -cleaned up lines

--- PopsCars/Service.cs
+++ PopsCars/Service.cs
@@ -8,21 +8,17 @@
             data = new Data();
         }
         public List<Cars> GetCarById(int id)
-        {
-            
+        {           
             return data.carList.Where(c => c.id.ToString().Contains(id.ToString())).ToList();
         }
 
         public List<Cars> GetCarsByYear(int year)
-        {
-            
+        {           
             return data.carList.Where(c => c.year.ToString().Contains(year.ToString())).ToList();
-
         }
 
         public List<Cars> GetAllCars()
-        {
-            
+        {            
             return data.carList;
         }
 
@@ -32,14 +28,12 @@
         }
 
         public List<Cars> GetCarsByMake(string make)
-        {
-            
+        {            
             return data.carList.Where(c => c.make.Contains(make, StringComparison.InvariantCultureIgnoreCase)).ToList();
         }
 
         public List<Cars> GetCarsByColor(string color)
-        {
-            
+        {           
             return data.carList.Where(c => c.color.Contains(color, StringComparison.InvariantCultureIgnoreCase)).ToList();
         }
 
