ca26170d30a4124610d1e5616dc89fc9a5b44d27
riosa <143053042+SirAjax@users.noreply.github.com>
2023-10-03 21:22:22 -0400

    -Added Note Repository

---
+++ EFTest/NoteRepository.cs
@@ -0,0 +1,51 @@
+﻿using EFTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest
+{
+	internal class NoteRepository
+	{
+		private readonly PopsCarsContext _popsCarsContext;
+
+		public NoteRepository(PopsCarsContext popsCarsContext)
+		{
+			_popsCarsContext = popsCarsContext;
+		}
+
+		public Note CreateNote(Note note) 
+		
+		{ 
+			_popsCarsContext.Note.Add(note);
+			_popsCarsContext.SaveChanges();
+			return note;
+		}
+
+		public List<Note> GetAllNotes() 
+		{ 
+			return _popsCarsContext.Note.ToList();	
+		}
+
+		public List<Note> GetComments(string comments) 
+		{
+			return _popsCarsContext.Note.Where(c => c.Comments == comments).ToList();
+		}
+
+		public Note UpdateComments(Note comments) 
+		{
+			Note? currentComments = _popsCarsContext.Note.Find(comments);
+			_popsCarsContext.Entry(currentComments).CurrentValues.SetValues(comments);
+			_popsCarsContext.SaveChanges();
+			return comments;
+
+		} 
+		public void DeleteNote(Note note) 
+		{
+			_popsCarsContext.Note.Remove(note);
+			_popsCarsContext.SaveChanges();
+		}
+	}
+}
