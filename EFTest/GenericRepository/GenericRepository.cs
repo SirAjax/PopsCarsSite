ffe14fb9b22873d39a9440dc6370aed705a507fb
riosa <143053042+SirAjax@users.noreply.github.com>
2023-11-26 19:46:01 -0500

    -created  a Generic Repository Class and Interface

---
+++ EFTest/GenericRepository/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFTest;
+
+namespace EFTest.GenericRepository
+{
+    
+
+    public (DatabaseContext ctx)
+        
+       
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public bool Add(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EFTest/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest.GenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        bool Add(T model);
+        bool Update(T model);
+        T GetById(int id);
+        
+        bool Delete(T model);
+    }
+}
