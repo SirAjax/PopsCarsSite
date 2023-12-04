8a9247f75a328837d7be6d6fbb6a05ce2e850e44
riosa <143053042+SirAjax@users.noreply.github.com>
2023-12-03 22:33:39 -0500

    -fixed

--- EFTest/UserRepository.cs
+++ EFTest/UserRepository.cs
@@ -1,50 +1,16 @@
+
 ﻿using EFTest.Models;
 using Microsoft.EntityFrameworkCore;
 
+﻿using EFTest.GenericRepository;
+using EFTest.Models;
+
+
 namespace EFTest;
-public class UserRepository : IUserRepository
-{
-	private readonly PopsCarsContext _popsCarsContext;
-
-	public UserRepository(PopsCarsContext popsCarsContext)
+public class UserRepository : GenericRepository<User>, IUserRepository 
+{	
+	public UserRepository(PopsCarsContext popsCarsContext) : base(popsCarsContext)
 	{
-		_popsCarsContext = popsCarsContext;
-	}
-
-	public async Task<User> CreateUser(User user)
-	{
-		_popsCarsContext.Add(user);
-		await _popsCarsContext.SaveChangesAsync();
-		return user;
-	}
-	public async Task<List<User>> GetAllUserAsync()
-	{
-		return await _popsCarsContext.User.ToListAsync();
-	}
-
-	public async Task<List<User>> GetUserByName(string userName)
-	{
-		return await _popsCarsContext.User.Where(a => a.UserName == userName).ToListAsync();
-	}
-
-	public User? GetUserObject(string userName)
-	{
-		return _popsCarsContext.User.Where(a => a.UserName == userName).FirstOrDefault();
-	}
-
-	public User UpdateUser(User user)
-	{
-		User? currentUser = _popsCarsContext.User.Find(user.ID);
-		_popsCarsContext.Entry(currentUser!).CurrentValues.SetValues(user);
-		_popsCarsContext.SaveChanges();
-		return user;
-	}
-
-	public void DeleteUser(User user)
-	{
-		User? currentUser = _popsCarsContext.User.OrderBy(i => i.ID).FirstOrDefault(a => a.UserName == user.UserName);
-		_popsCarsContext.Remove(currentUser!);
-		_popsCarsContext.SaveChanges();
 	}
 
 }
