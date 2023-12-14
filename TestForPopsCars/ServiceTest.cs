65cb8ab2318240dcb9beaa3311077dda93147776
AJ Rios <arios@wrbts.ads.wrberkley.com>
2023-12-14 17:55:45 -0500

    -reformatting service tests

--- TestForPopsCars/ServiceTest.cs
+++ TestForPopsCars/ServiceTest.cs
@@ -8,50 +8,55 @@ namespace TestForPopsCars
     {
         private Service service;
         private Mock<ICarsRepository> mockRepo;
-        private Car car = new Car { Year = 1980, Make = "unit test", Model = "Test", Color = "Blue"};
-
+        
         [TestInitialize]
         public void Setup()
         {
             mockRepo = new Mock<ICarsRepository>();
             service = new Service(mockRepo.Object);
-            mockRepo.Setup(repo => repo.GetAll()).Returns(new List<Car> { new Car { Year = 5, Make = "unit test", Model = "TEST", Color = "Turqouise"} });
-
+      
         }
         [TestMethod]
-        public void Does_AddCar_Return_Success()
+        public async Task Does_AddCar_Return_Success()
         {
-            var newCar = service.AddCar(car);
-            Assert.IsNotNull(newCar);
+            //              It.IsAny<T>()
+
+            mockRepo.Setup(repo => repo.Add(It.IsAny<Car>())).Returns(true);
+            Car car = new Car { Year = 1980, Make = "unit test", Model = "Test", Color = "Blue" };
+            var newCar = await service.AddCar(car);
+            Assert.IsTrue(newCar);
         }
 
-        [TestMethod]
-        public void Does_GetAllCars_Return_Success()
-        {
-            var result = service.GetAllCars();
-            Assert.AreEqual(1, result.Count);
-        }
-
-        [TestMethod]
-        public void Does_DeleteCar_Return_Success()
-        {
-            var result = service.DeleteCar(car);
-            Assert.IsNotNull(result);
-        }
-        [TestMethod]
-        public void Does_UpdateCar_Return_Success()
-        {
-            var result = service.UpdateCar(car);
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-
-        public void Does_MainSearch_Return_Success()
-        {
-            var result = service.MainSearch("unit test");
-            Assert.IsNotNull(result);
-        }
+    [TestMethod]
+    public void Does_GetAllCars_Return_Success()
+    {
+        var result = service.GetAllCars();
+        Assert.AreEqual(1, result.Count);
     }
+
+    [TestMethod]
+    public async Task Does_DeleteCar_Return_Success()
+    {
+            mockRepo.Setup(repo => repo.Delete(It.IsAny<Car>())).Returns(true);
+            Car car = new Car { Year = 1980, Make = "unit test", Model = "Test", Color = "Blue" };
+            var result = await service.DeleteCar(car);
+            Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void Does_UpdateCar_Return_Success()
+    {
+        var result = service.UpdateCar(car);
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+
+    public void Does_MainSearch_Return_Success()
+    {
+        var result = service.MainSearch("unit test");
+        Assert.IsNotNull(result);
+    }
+}
 }
 
