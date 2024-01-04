ce8938171170ec0c7fa5be7ed9a403e557945d79
riosa <143053042+SirAjax@users.noreply.github.com>
2024-01-04 00:31:18 -0500

    -made changes to initialUser code behind

--- PopsCarsSite/Pages/InitialUser.razor.cs
+++ PopsCarsSite/Pages/InitialUser.razor.cs
@@ -2,6 +2,7 @@ using EFTest.Models;
 using Microsoft.AspNetCore.Components;
 using PopsCars;
 
+
 namespace PopsCarsSite.Pages
 {
 	public class InitialUserComponent : ComponentBase
@@ -10,20 +11,49 @@ namespace PopsCarsSite.Pages
 		protected List<Car> ListOfCars = new();
 		protected List<User> ListOfUsers = new();
 		protected List<Note> ListOfNotes = new();
-		protected Note? newNote = new Note();
-
+		protected Car? newCar = new();
+        protected Car? carToUpdate = new();
+		protected Car? carToDelete = new();
+        protected Note? newNote = new();
+		protected string search;
 		[Inject]
 		private IService _service { get; set; } = default!;
 		protected override async Task OnInitializedAsync()
 		{
 			await PopulateCarList();
 			await PopulateUserList();
+			await PopulateNoteList();
 		}
 		protected async Task PopulateCarList()
 		{
 			ListOfCars = await _service.GetAllCars();
 		}
 		
+		protected async Task AddCar()
+		{
+			await _service.AddCar(newCar);
+		}
+        protected async Task FilterBySearch()
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                Console.WriteLine("no search results");
+            }
+            else
+            {
+                ListOfCars = await _service.MainSearch(search);
+            }
+        }
+        protected async Task UpdateCar()
+		{
+			await _service.UpdateCar(carToUpdate);
+		}
+
+		protected async Task DeleteCar()
+		{
+			await _service.DeleteCar(carToDelete);
+		}
+
 		[Inject]
 		private IUserService _userservice { get; set; } = default!;
 		protected async Task PopulateUserList()
