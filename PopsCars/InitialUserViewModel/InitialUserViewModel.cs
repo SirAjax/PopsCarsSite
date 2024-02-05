9071d25324dad3ca40465c4710760fa8fba60007
AJ <rios.alex@hotmail.com>
2024-02-04 23:25:01 -0500

    -made changes to null status of items in InitialUserViewModel

--- PopsCars/InitialUserViewModel/InitialUserViewModel.cs
+++ PopsCars/InitialUserViewModel/InitialUserViewModel.cs
@@ -7,14 +7,14 @@ namespace PopsCars
 		
 		public List<Car> ListOfCars = new();
 		public List<Note> ListOfNotes = new();
-		public Car? newCar = new();
-		public Car? carToUpdate = new();
-		public Car? carToDelete = new();
+		public Car newCar = new();
+		public Car carToUpdate = new();
+		public Car carToDelete = new();
 		public int initialUser { get; set; } = 1;
-		public User? currentUser = new();
-		public Note? newNote = new();
-		public Note? noteToUpdate = new();
-		public Note? noteToDelete = new();
+		public User currentUser = new();
+		public Note newNote = new();
+		public Note noteToUpdate = new();
+		public Note noteToDelete = new();
 		public string search;
 		public bool isSortedById = true;
 		public string sortButtonText;
